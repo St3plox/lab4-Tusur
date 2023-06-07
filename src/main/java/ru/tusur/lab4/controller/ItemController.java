e48fe3a439a0e903b87b92aaa6c6afc0f7193f81
JegorSt3p <st3pegor@gmail.com>
2023-06-07 15:00:06 +0700

    Dockerfile

--- src/main/java/ru/tusur/lab4/controller/ItemController.java
+++ src/main/java/ru/tusur/lab4/controller/ItemController.java
@@ -1,5 +1,6 @@
 package ru.tusur.lab4.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,35 +18,43 @@ public class ItemController {
     ItemService itemService;
 
     @Tag(name = "Crud Operation")
+    @Operation(summary = "Возвращает продукт", description = "возращает продукт используя id")
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItem(@PathVariable Long id) {
         return new ResponseEntity<>(itemService.getItem(id), HttpStatus.OK);
     }
 
     @Tag(name = "Crud Operation")
+    @Operation(summary = "Сохраняет проlукт", description = "Сохраняет продукт, принимая json item")
     @PostMapping
     public ResponseEntity<Item> saveItem(@RequestBody Item item) {
         return new ResponseEntity<>(itemService.saveItem(item), HttpStatus.CREATED);
     }
 
     @Tag(name = "Crud Operation")
+    @Operation(summary = "Обновляет продукт", description = "Обновляет продукт по зажанному id")
     @PutMapping("/{id}")
     public ResponseEntity<Item> updateItem(@RequestBody Item item) {
         return new ResponseEntity<>(itemService.saveItem(item), HttpStatus.OK);
     }
 
     @Tag(name = "Crud Operation")
+    @Operation(summary = "Удаляет продукт", description = "Удаляет продукт, используя ID")
     @DeleteMapping("/{id}")
-    public ResponseEntity updateItem(@PathVariable Long id) {
+    public ResponseEntity deleteItem(@PathVariable Long id) {
         itemService.deleteItem(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Tag(name = "Statistics")
+    @Operation(summary = "Возвращает среднюю стоимость", description = "Возвращает среднюю стоимость среди всех продуктов")
     @GetMapping("/avg")
     public ResponseEntity<Double> getAvgPrice() {
         return new ResponseEntity<>(itemService.getAveragePrice(), HttpStatus.OK);
     }
 
+    @Tag(name = "Statistics")
+    @Operation(summary = "Возвращает максимальную стоимость", description = "Возвращает максимальную стоимость среди всех продуктов")
     @GetMapping("/max")
     public ResponseEntity<Double> getMaxPrice() {
         return new ResponseEntity<>(itemService.getMaxPrice(), HttpStatus.OK);
